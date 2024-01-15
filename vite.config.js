c6feec22fa8a46377d3d411624fd8cdc9a22293d
juanpinto1 <juan.pinto@cajalosandes.cl>
2024-01-15 01:21:20 -0300

    1

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'https://juanpinto1.github.io/reactclase1/'
+  base:'/reactclase1/'
 })
